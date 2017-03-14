94e784de4ebec04e6b87517c55f3afaca18250f2
Adam Stankiewicz <sheerun@sher.pl>
2017-03-14 19:08:09 +0100

    Properly recognize default location of eslint

--- src/parser.js
+++ src/parser.js
@@ -10,21 +10,7 @@ const parser = yargs
   .alias('h', 'help')
   .version()
   .options({
-    write: {
-      default: false,
-      describe: 'Edit the file in-place (beware!)',
-      type: 'boolean',
-    },
     stdin: {default: false, describe: 'Read input via stdin', type: 'boolean'},
-    'eslint-ignore': {
-      default: true,
-      type: 'boolean',
-      describe: oneLine`
-        Only format matching files even if
-        they are not ignored by .eslintignore.
-        (can use --no-eslint-ignore to disable this)
-      `,
-    },
     eslintPath: {
       default: getPathInHostNodeModules('eslint'),
       describe: 'The path to the eslint module to use',
@@ -49,20 +35,6 @@ const parser = yargs
       alias: 'l',
       default: 'warn',
     },
-    // TODO: if we allow people to to specify a config path,
-    // we need to read that somehow. These can come invarious
-    // formats and we'd have to work out `extends` somehow as well.
-    // I don't know whether ESLint exposes a way to do this...
-    // Contributions welcome!
-    // eslintConfigPath: {
-    //   describe: 'Path to the eslint config to use for eslint --fix',
-    // },
-    // TODO: would this be just a JSON file? There's never going to be
-    // a `.prettierrc`: https://github.com/jlongster/prettier/issues/154
-    // so we'll have to be careful how we do this (if we do it at all).
-    // prettierOptions: {
-    //   describe: 'Path to the prettier config to use',
-    // },,
   })
   .strict();
 
@@ -73,7 +45,7 @@ function getPathInHostNodeModules(module) {
   if (modulePath) {
     return modulePath;
   } else {
-    return path.relative(__dirname, `../node_modules/${module}`);
+    return path.resolve(__dirname, `../node_modules/${module}`);
   }
 }
 
