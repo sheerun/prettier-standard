d5099fa6a2b56ce863bf125c2b10e58e5231eeff
Adam Stankiewicz <sheerun@sher.pl>
2017-06-12 23:00:13 +0200

    Remove unnecessary line

--- src/no-main.js
+++ src/no-main.js
@@ -3,7 +3,6 @@ const message = `
 Looks like you're trying to require/import \`prettier-standard\`.
 This module doesn't actually expose a NodeJS interface.
 This module's just the CLI for \`prettier-standard\`.
-If you want to use this via NodeJS, install \`prettier-standard\` instead.
 `.trim()
 
 console.info(message)
